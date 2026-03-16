2daf09fd8d4ac28a558d7ced00e0637aebeaaa25
Alex <laurelone666@gmail.com>
2026-03-16 14:22:17 +0300

    Откатить удаленный коммит и синхрони

--- tasks/variables/Variables_04.py
+++ tasks/variables/Variables_04.py
@@ -23,3 +23,12 @@
 """
 
 # Напиши решение ниже.
+
+disk_total_gb = 512
+disk_used_gb = 378.4
+
+disk_free_gb = disk_total_gb - disk_used_gb
+percent_used = disk_used_gb / disk_total_gb * 100
+percent_free = disk_free_gb / disk_total_gb * 100
+
+print(f"Использовано: {disk_used_gb:.2f} GB ({percent_used:.2f}%), свободно: {disk_free_gb:.2f} GB ({percent_free:.2f}%)")
